1b5d8fc0543aed2126d54d70e2f0c01ad89f6a68
bjanssens <bjanssens@inuits.eu>
2015-03-31 10:13:49 +0200

    Trying to fix permission error
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- puppet/dev/manifests/graphite-web.pp
+++ puppet/dev/manifests/graphite-web.pp
@@ -1,3 +1,12 @@
 node 'graphite-web' {
-  notify("I am alive")
+  notify { 'Alive notify':
+    message => "Node ${::hostname} is alive!",
+  }
+
+  file { 'Fix for permission error':
+    ensure  => directory,
+    path    => '/home/vagrant/.ssh',
+    mode    => '0700',
+    recurse => true,
+  }
 }
