c5af212d4f87cf86211439da8252ea6d77f0c66e
bjanssens <bjanssens@inuits.eu>
2015-04-02 10:58:13 +0200

    Bugfix: Use class to do the setup stuff
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- puppet/dev/manifests/site.pp
+++ puppet/dev/manifests/site.pp
@@ -7,23 +7,10 @@ Package { allow_virtual => false }
 Yumrepo <| |> -> User <| |> -> Package <| |>
 
 #Stuff that every node needs like the epel repo.
-
 stage { 'setup':
   before => Stage['main'],
 }
 
-yumrepo { 'epel':
-  ensure         => present,
-  stage          => 'setup',
-  descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
-  mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
-  failovermethod => 'priority',
-  enabled        => 1,
-  gpgcheck       => 1,
-  gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
-}
-
-$packages = ['tree']
-package { $packages:
-  ensure => present,
+class { 'setup':
+  stage => 'setup',
 }
