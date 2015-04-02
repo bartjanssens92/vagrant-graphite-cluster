8f6d972c2fc4e6e926f2b23395bdae2a12a0d806
bjanssens <bjanssens@inuits.eu>
2015-04-02 10:45:41 +0200

    Added Repo first stage hack
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- puppet/dev/manifests/site.pp
+++ puppet/dev/manifests/site.pp
@@ -7,8 +7,14 @@ Package { allow_virtual => false }
 Yumrepo <| |> -> User <| |> -> Package <| |>
 
 #Stuff that every node needs like the epel repo.
+
+stage { 'setup':
+  before => Stage['main'],
+}
+
 yumrepo { 'epel':
   ensure         => present,
+  stage          => 'setup'
   descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
   mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
   failovermethod => 'priority',
