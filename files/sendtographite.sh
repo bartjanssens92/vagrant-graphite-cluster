c4c92fa521424b1a98dcb12654218cbe9ed67006
bjanssens <bjanssens@inuits.eu>
2015-05-04 16:59:21 +0200

    Update submodule reference once again
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- files/sendtographite.sh
+++ files/sendtographite.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 while true
 do
-  for i in {1..10}
+  for i in {1..100}
   do
-    for d in {1..10}
+    for d in {1..100}
     do
       echo "local.test${i}.test${d} 4 `date +%s`" | nc localhost 2003
     done
