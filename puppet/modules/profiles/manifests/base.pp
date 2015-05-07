7db5e77f12143ec5f101ad657914f64512300b0b
bjanssens <bjanssens@inuits.eu>
2015-05-07 08:54:51 +0200

    Removed the collectd module
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- puppet/modules/profiles/manifests/base.pp
+++ puppet/modules/profiles/manifests/base.pp
@@ -5,19 +5,4 @@ class profiles::base {
   notify { 'Alive notify':
     message => "Node ${::hostname} is alive!",
   }
-
-  class { '::collectd':
-    purge        => true,
-    recurse      => true,
-    purge_config => true,
-  }
-
-  class { 'collectd::plugin::write_graphite':
-    graphitehost => '192.168.1.103',
-  }
-
-  class { 'collectd::plugin::disk':
-    disks          => ['/^dm/'],
-    ignoreselected => true
-  }
 }
